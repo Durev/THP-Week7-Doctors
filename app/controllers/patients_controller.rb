3ed3992e45d8b6cc87554ce3eb94ab897814db8b
Durev <vincent.durewski@gmail.com>
2017-08-18 17:08:15 +0200

    Add patient creation form and patients index

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,26 @@
 class PatientsController < ApplicationController
+  
   def new
+  	@patient = Patient.new
+  	@patients = Patient.all
   end
+
+  def confirmation
+  end
+
+  def create
+  	@patient = Patient.new(patient_params)
+  	if @patient.save
+  		redirect_to signup_path
+  	else
+  		render '/signup'
+  	end
+  end
+
+  private
+
+  def patient_params
+  	params.require(:patient).permit(:first_name, :last_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'patients#new'
 
-  get 'patients/new'
+  get '/signup', to: 'patients#new'
+  get '/confirmation', to: 'patients#confirmation'
+  post '/signup', to: 'patients#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
